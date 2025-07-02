55da7fa87af7389ed5f889ad028f7a196d392f30
vlada-marchenko <vvvvadya44@gmail.com>
2025-07-02 17:23:18 +0200

    fix e747827

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -3,17 +3,15 @@ import css from './LayoutNotes.module.css';
 type Props = {
   children: React.ReactNode;
   sidebar: React.ReactNode;
-  modal: React.ReactNode;
 };
 
-export default function NotesLayout({ children, sidebar, modal }: Props) {
+export default function NotesLayout({ children, sidebar }: Props) {
   return (
     <>
       <div className={css.container}>
         <aside className={css.sidebar}>{sidebar}</aside>
         <section className={css.notesWrapper}>{children}</section>
       </div>
-      {modal}
     </>
   );
 }
