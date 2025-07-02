7d249d088cf4c8ca908e0a12649681f77120d55f
vlada-marchenko <vvvvadya44@gmail.com>
2025-07-02 04:25:00 +0200

    take 2414124

--- app/notes/filter/@modal/(..)[id]/page.tsx
+++ app/notes/filter/@modal/(..)[id]/page.tsx
@@ -2,11 +2,11 @@ import Modal from '../../../../../components/Modal/Modal';
 import NotePreview from '../../../../../components/NotePreview/NotePreview';
 import { fetchNoteById } from '../../../../../lib/api';
 
-export default async function NoteModalPage({
-  params,
-}: {
+type PageParams = {
   params: { id: string };
-}) {
+};
+
+export default async function NoteModalPage({ params }: PageParams) {
   const note = await fetchNoteById(Number(params.id));
 
   return (
