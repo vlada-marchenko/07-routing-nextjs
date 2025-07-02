ee08e50df64beaf1e4e3c73079f698af7ce74c1b
vlada-marchenko <vvvvadya44@gmail.com>
2025-07-02 17:29:55 +0200

    vvvvv

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Modal from '../../../../components/Modal/Modal';
-import NotePreview from './NotePreview';
+import NotePreview from './NotePreview.client';
 import { fetchNoteById } from '../../../../lib/api';
 
 type Params = Promise<{ id: string }>;
