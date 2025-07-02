9c88dc59176cdf8ab102443fab9f6ce824471889
vlada-marchenko <vvvvadya44@gmail.com>
2025-07-02 13:05:03 +0200

    minor fix

--- app/notes/filter/@modal/(..)[id]/page.tsx
+++ app/notes/filter/@modal/(..)[id]/page.tsx
@@ -2,11 +2,11 @@ import Modal from '../../../../../components/Modal/Modal';
 import NotePreview from '../../../../../components/NotePreview/NotePreview';
 import { fetchNoteById } from '../../../../../lib/api';
 
-type PageParams = {
+export default async function NoteModalPage({
+  params,
+}: {
   params: { id: string };
-};
-
-export default async function NoteModalPage({ params }: PageParams) {
+}) {
   const note = await fetchNoteById(Number(params.id));
 
   return (
