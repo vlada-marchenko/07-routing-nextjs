e2c545cf988c952273c1004dcb48aed22aef21f4
vlada-marchenko <vvvvadya44@gmail.com>
2025-07-02 16:07:55 +0200

    fix modal page

--- app/notes/filter/@modal/(..)[id]/page.tsx
+++ app/notes/filter/@modal/(..)[id]/page.tsx
@@ -3,18 +3,15 @@ import NotePreview from '../../../../../components/NotePreview/NotePreview';
 import { fetchNoteById } from '../../../../../lib/api';
 
 interface NoteModalPageProps {
-  params: { id: string };
+  params: { id: string };
 }
 
-export default async function NoteModalPage(
-  props: NoteModalPageProps | Promise<NoteModalPageProps>
-) {
-  const { params } = await props;
-  const note = await fetchNoteById(Number(params.id));
+export default async function NoteModalPage({ params }: NoteModalPageProps) {
+  const note = await fetchNoteById(Number(params.id));
 
-  return (
-    <Modal>
-      <NotePreview note={note} />
-    </Modal>
-  );
+  return (
+    <Modal>
+      <NotePreview note={note} />
+    </Modal>
+  );
 }
